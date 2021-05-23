5c177d051990697eb363f1fece5eb551ff4440b6
Jordan Masone <jordanmasone1@gmail.com>
2021-05-23 14:58:05 -0400

    Added styling too search modal.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,19 @@ import { Navbar } from 'react-bootstrap'
 
 export default function NavBar({ light }) {
 
-    const lightStyle = { backgroundColor: "#344455" };
+    const lightStyle = { backgroundColor: "#6c757d" };
     const darkStyle = { backgroundColor: "#E5383B" };
 
     return (
         <>
             {light === false && (
                 <Navbar variant="dark" expand="lg0" style={darkStyle}>
-                    <Navbar.Brand href="/">Pokedex</Navbar.Brand>
+                    <Navbar.Brand href="/">React Pokedex</Navbar.Brand>
                 </Navbar>
             )}
             {light === true && (
                 <Navbar variant="dark" expand="lg0" style={lightStyle}>
-                    <Navbar.Brand href="/">Pokedex</Navbar.Brand>
+                    <Navbar.Brand href="/">React Pokedex</Navbar.Brand>
                 </Navbar>
             )}
         </>
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,23 +1,61 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button, Jumbotron } from 'react-bootstrap';
+import { Container, Row, Col, Button, Jumbotron, Modal, Form } from 'react-bootstrap';
 import NavBar from '../components/NavBar';
 import Wrapper from "../components/Wrapper";
 import configs from "../utils/backgroundConfig";
+import { useHistory } from "react-router-dom";
+import API from "../utils/API";
 
 export default function Test() {
 
-    const [config, setConfig] = useState(configs.lightConfig);
+    const history = useHistory();
+    const [show, setShow] = useState(false);
 
+    // Styles
+    const [config, setConfig] = useState(configs.lightConfig);
     const cardStyle = {
         position: "relative",
         top: "300px",
         width: "auto",
-        backgroundColor: "#344455"
+        backgroundColor: "#6c757d"
     };
-
     const buttonStyle = {
         margin: "10px"
     }
+
+    const changePage = (e, poke) => {
+        e.preventDefault();
+        history.push(`/info/${poke}`);
+    }
+
+    // Function to find random pokemon
+    async function handleRandomClick(e) {
+        e.preventDefault();
+
+        let pokemon = await API.getAllPokemon();
+
+        // Generate random number based on size of array.
+        // Grab a random array value.
+        // Capitilize the first letter of the name.
+        let num = Math.floor(Math.random() * pokemon.data.results.length);
+        let random = pokemon.data.results[num];
+        let upperCase = random.name.charAt(0).toUpperCase() + random.name.slice(1);
+
+        // Get Data on individual pokemon with url link
+        changePage(e, upperCase);
+
+    };
+
+    // Modal Functions
+    const handleButtonClick = () => {
+
+        if (show === false) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
+    }
+
 
     return (
         <div>
@@ -32,11 +70,35 @@ export default function Test() {
                                 <h5 style={{ color: "#fff" }}>Fully updated with all Pokemon from Gen I - Gen VIII</h5>
                                 <div>
                                     <Button style={buttonStyle} type="button" variant="outline-danger" size="lg">Full Pokedex</Button>
-                                    <Button style={buttonStyle} type="button" variant="outline-danger" size="lg">Surprise Me</Button>
-                                    <Button style={buttonStyle} type="button" variant="outline-danger" size="lg">Search by Name/Number</Button>
+                                    <Button style={buttonStyle} type="button" variant="outline-danger" size="lg" onClick={(e) => handleRandomClick(e)}>Surprise Me</Button>
+                                    <Button style={buttonStyle} type="button" variant="outline-danger" size="lg" onClick={() => handleButtonClick()}>Search by Name/Number</Button>
                                 </div>
                             </Container>
                         </Jumbotron>
+                        <Modal show={show} onHide={() => handleButtonClick()}>
+                            <Modal.Header style={{ backgroundColor: "#6c757d" }} >
+                                <Modal.Title style={{ color: "#fff" }}>Pokemon Search</Modal.Title>
+                            </Modal.Header>
+                            <form>
+                                <Modal.Body style={{ backgroundColor: "#6c757d", color: "#fff" }}>
+                                    <Form>
+                                        <Row>
+                                            <Col style={{ borderRight: "1px solid" }}>
+                                                <Form.Label style={{ marginRight: "10px" }}><u>By Name</u></Form.Label>
+                                                <Form.Control type="text" placeholder="Pikachu" />
+                                            </Col>
+                                            <Col>
+                                                <Form.Label style={{ marginRight: "10px" }}><u>By Number</u></Form.Label>
+                                                <Form.Control type="text" placeholder="44" />
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                </Modal.Body>
+                                <Modal.Footer style={{ backgroundColor: "#6c757d" }}>
+                                    <Button type="submit" variant="outline-danger">Search!</Button>
+                                </Modal.Footer>
+                            </form>
+                        </Modal>
                     </Col>
                 </Row>
             </Container>
